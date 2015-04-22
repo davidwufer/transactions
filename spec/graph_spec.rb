c5c4c10cd08b36f45ed402d48b7887485e1cf7d6
David Wu <davidcnwu@gmail.com>
2015-04-22 01:32:01 -0700

    Fixing tests and implementing behavior

--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -8,74 +8,76 @@ describe Graph do
 
   describe "TransactionData reading" do
     it "converts a transaction data into nodes" do
-      graph.nodes.should include("David", "Angela")
+      expect(graph.nodes).to include("David", "Angela")
     end
 
     it "adds the right amount to ower lookup" do
-      graph.owed_amount("David", "Angela").should == 200
+      expect(graph.owed_amount("David", "Angela")).to eq(200)
     end
 
     it "adds the right amount to owed lookup" do
-      graph.owed_amount("Angela", "David").should == -200
+      expect(graph.owed_amount("Angela", "David")).to eq(-200)
     end
 
     it "returns the right amount for nonexistent owers" do
-      graph.owed_amount("Peter", "Angela").should == 0
+      expect(graph.owed_amount("Peter", "Angela")).to eq(0)
     end
 
     it "returns the right amount for nonexistent owed people" do
-      graph.owed_amount("David", "Janet").should == 0
+      expect(graph.owed_amount("David", "Janet")).to eq(0)
     end
   end
 
   describe "Graph modification" do
     it "adds a new edge with the right amount" do
+      graph.add_node("Baran")
+
       graph.add_edge("David", "Baran", 200)
-      graph.owed_amount("David", "Baran").should == 200
-      graph.owed_amount("Baran", "David").should == -200
+      expect(graph.owed_amount("David", "Baran")).to eq(200)
+      expect(graph.owed_amount("Baran", "David")).to eq(-200)
     end
 
     it "adds to an existing edge with the right amount" do
       graph.add_edge("David", "Angela", 200)
-      graph.owed_amount("David", "Angela").should == 400
+      expect(graph.owed_amount("David", "Angela")).to eq(400)
     end
 
     it "adds to an existing reversed edge with the right amount" do
       graph.add_edge("Angela", "David", 200)
-      graph.owed_amount("David", "Angela").should == 0
+      expect(graph.owed_amount("David", "Angela")).to eq(0)
     end
 
     it "removes an edge if the amount reaches 0" do
       graph.add_edge("Angela", "David", 200)
-      graph.has_edge?("David", "Angela"). should be_false
-      graph.has_edge?("Angela", "David"). should be_false
+      expect(graph.has_edge?("David", "Angela")).to be false
+      expect(graph.has_edge?("Angela", "David")).to be false
     end
 
     it "reverses an edge if the amount added exceeds the negative of the amount already there" do
       graph.add_edge("Angela", "David", 400)
-      graph.owed_amount("David", "Angela").should == -200
-      graph.owed_amount("Angela", "David").should == 200
+      expect(graph.owed_amount("David", "Angela")).to eq(-200)
+      expect(graph.owed_amount("Angela", "David")).to eq(200)
     end
 
     describe "#remove_edge" do
       it "removes an edge" do
         graph.remove_edge("David", "Angela")
-        graph.has_edge?("David", "Angela").should be_false
-        graph.owed_amount("David", "Angela").should == 0
+        expect(graph.has_edge?("David", "Angela")).to be false
+        expect(graph.owed_amount("David", "Angela")).to eq 0
       end
 
       it "removes a negative edge" do
         graph.remove_edge("Angela", "David")
-        graph.has_edge?("David", "Angela").should be_false
-        graph.owed_amount("David", "Angela").should == 0
+        expect(graph.has_edge?("David", "Angela")).to be false
+        expect(graph.owed_amount("David", "Angela")).to be 0
       end
 
       it "returns the owed amount" do
-        graph_remove_edge("David", "Angela").should == 200
+        expect(graph.remove_edge("David", "Angela")).to eq(200)
       end
 
       it "returns the owed amount for a negative edge" do
-        graph_remove_edge("Angela", "David").should == -200
+        expect(graph.remove_edge("Angela", "David")).to eq(-200)
       end
     end
   end
--- transaction.rb
+++ transaction.rb
@@ -6,16 +6,51 @@ require './transaction_data'
 
 module Transaction
   class Graph
-    attr_reader :transaction_data
+    attr_reader :nodes
 
     def initialize(args={})
-      @transaction_data = args[:transaction_data] || []
-      initialize_nodes
+      @nodes = {}
+
+      transaction_data = args[:transaction_data] || []
+      initialize_nodes(transaction_data)
+    end
+
+    def add_node(node)
+      nodes[node] = Hash.new(0) unless nodes.has_key?(node)
+    end
+
+    def add_edge(ower, owed, amount)
+      nodes[ower][owed] += amount
+      nodes[owed][ower] -= amount
+
+      if nodes[ower][owed].zero?
+        nodes[ower].delete(owed)
+      end
+
+      if nodes[owed][ower].zero?
+        nodes[owed].delete(ower)
+      end
+    end
+
+    def remove_edge(owed, ower)
+      nodes[ower].delete(owed)
+      nodes[owed].delete(ower)
+    end
+
+    def has_edge?(ower, owed)
+      nodes[ower].has_key?(owed)
+    end
+
+    def owed_amount(ower, owed)
+      nodes.has_key?(ower) ? nodes[ower][owed] : 0
     end
 
     private
-      def initialize_nodes
-        transaction_data.each do |transaction_datum|
+      def initialize_nodes(transaction_data)
+        transaction_data.each do |datum|
+          add_node(datum.ower)
+          add_node(datum.owed)
+          add_edge(datum.ower, datum.owed, datum.amount)
         end
       end
   end
